cf53a30d2c76fe2d6c9fe60130e86fc314789131
eCasparGTech <ecaspar@gaming.tech>
2025-09-12 13:32:14 +0200

    feat: wave number

--- shmup/WaveNumber.cpp
+++ shmup/WaveNumber.cpp
@@ -8,12 +8,18 @@ WaveNumber::WaveNumber() {}
 void WaveNumber::start()
 {
     UI::start();
+    
+    mp_waveNumberText = mp_gameManager->createSprite();
+    mp_waveNumberText->setPosition({ -500.0f, -500.0f });
 }
 
-void WaveNumber::displayWaveNumber()
+void WaveNumber::showWaveNumber()
 {
-    Sprite* mp_waveNumberText = mp_gameManager->createSprite();
-    
+    mp_waveNumberText->setText("Wave " + std::to_string(mp_gameManager->m_wave + 1));
     mp_waveNumberText->setPosition({mp_gameManager->getWindow()->getDimensions().x * 0.5f, mp_gameManager->getWindow()->getDimensions().y * 0.5f});
-    mp_waveNumberText->setText(std::to_string(mp_gameManager->m_wave));
+}
+
+void WaveNumber::hideWaveNumber()
+{
+    mp_waveNumberText->setPosition({ -500.0f, -500.0f });
 }
